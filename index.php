810d2828a90dd984cd9e8f852222a467b82a974e
Peter Goodwin <s3793762@student.rmit.edu.au>
2024-10-10 19:44:29 +1000

    Adding debug to index

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
+// Enable error reporting for debugging
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Get the requested URL path
 $request_uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+// Debugging: Log the requested URL path
+error_log("Requested URI: " . $request_uri);
 
 // Switch case for routing based on the URL
 switch ($request_uri) {
@@ -25,4 +32,8 @@ switch ($request_uri) {
         break;
 }
 
+// Debugging: Log the selected page
+error_log("Selected Page: " . $page);
+
+// Include the selected page
 include $page;
