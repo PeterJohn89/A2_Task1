8be452718d66de73a56aa14c255ad9da90a5e561
Peter Goodwin <s3793762@student.rmit.edu.au>
2024-10-11 19:33:19 +1000

    Remove vector from git

--- index.php
+++ index.php
@@ -1,30 +1,22 @@
 <?php
-// Enable error reporting for debugging
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 // Get the requested URL path
 $request_uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
-// Debugging: Log the requested URL path
-error_log("Requested URI: " . $request_uri);
-
 // Switch case for routing based on the URL
-var_dump($request_uri);
 switch ($request_uri) {
     case "/":
         $page = "login.php"; 
         break;
-    case "/A2_Task1/index.php":
+    case "/index":
         $page = "login.php"; 
         break;
-    case "/A2_Task1/login.php":
+    case "/login":
         $page = "login.php";
         break;
-    case "/A2_Task1/register.php":
+    case "/register":
         $page = "register.php";
         break;
-    case "/A2_Task1/main.php":
+    case "/main":
         $page = "main.php";
         break;
     default:
@@ -33,8 +25,4 @@ switch ($request_uri) {
         break;
 }
 
-// Debugging: Log the selected page
-error_log("Selected Page: " . $page);
-
-// Include the selected page
 include $page;
